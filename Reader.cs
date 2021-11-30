9c1515fd978d5acc0fb3c93c8249e5cb3d9c641c
Daan van Dongen <daanvandongen1999@gmail.com>
2021-11-30 15:08:48 +0100

    Fixed small parsing error

--- Reader.cs
+++ Reader.cs
@@ -9,7 +9,7 @@ public class Reader
     {
         // The day parameter has to be in the form of XY, where X is in {0..2} and Y is in {0..9}
         if (!int.TryParse(day, out int dayNr)) throw new ArgumentException("The argument needs to be an integer with two digits.");
-        if (dayNr < 0 || dayNr > 25)           throw new ArgumentOutOfRangeException();
+        if (dayNr < 1 || dayNr > 25)           throw new ArgumentOutOfRangeException();
 
         StreamReader sr = new StreamReader($"Input/Day{day}");
         List<string> result = new List<string>();
